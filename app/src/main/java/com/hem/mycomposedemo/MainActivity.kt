5edefbba1df0b1327c1bd71c78f0b895412f2fa0
hemashreedalvi1@gmail.com <hemashreedalvi1@gmail.com>
2022-11-16 11:39:07 +0530

    added compose function.

--- app/src/main/java/com/hem/mycomposedemo/MainActivity.kt
+++ app/src/main/java/com/hem/mycomposedemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hem.mycomposedemo.ui.theme.MyComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +40,11 @@ fun Greeting(name: String) {
     Surface(color = Color.Cyan) {
         Text(text = "Hello $name!", modifier = Modifier.padding(5.dp))
     }
+}
+
+@Composable
+fun BirthdayWish(msg: String, from: String) {
+    Text(text = "$msg", fontSize = 14.sp)
 }
 
 @Preview(showBackground = true)
